737299f4d3c70b2576b71ee1ceb9bb93edab2dd6
kortschak <dan.kortschak@adelaide.edu.au>
2013-06-06 09:19:10 +0930

    Make it possible to go get the talk
    This ensures the dependencies for play slides are installed.

---
+++ BioInfoSummer2012/bioinfosummer2012.go
@@ -0,0 +1,16 @@
+// Copyright ©2012 The bíogo.talks Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package bioinfosummer2012
+
+import (
+	_ "code.google.com/p/biogo.cluster"
+	_ "code.google.com/p/biogo.cluster/kmeans"
+	_ "code.google.com/p/biogo.external/muscle"
+	_ "code.google.com/p/biogo/alphabet"
+	_ "code.google.com/p/biogo/io/seqio/fasta"
+	_ "code.google.com/p/biogo/seq"
+	_ "code.google.com/p/biogo/seq/linear"
+	_ "code.google.com/p/biogo/seq/multi"
+)
